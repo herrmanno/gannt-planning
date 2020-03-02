f53dbe77b6695f5ea7409a724022ea9a66f7815a
Oliver Herrmann <herrmann@id-on.de>
2020-03-02 09:20:04 +0100

    Unselect event if no longer visible

--- src/redux/reducers/ui/selectedEventID.ts
+++ src/redux/reducers/ui/selectedEventID.ts
@@ -1,16 +1,27 @@
 import State from "../../State"
 import { SelectEvent } from "../../actions/ui/selectEvent"
+import { LoadEvents } from "../../actions/events/loadEvents"
 
 export default reducer
 
 type ReducerState = State["ui"]["selectedEventID"]
-type Action = SelectEvent
+type Action = SelectEvent | LoadEvents
 const defaultState: ReducerState = null
 
 
 function reducer(state = defaultState, action: Action): ReducerState {
     switch (action.type) {
-        case "SELECT_EVENT": return action.payload.id
+        case "SELECT_EVENT": {
+            return action.payload.id
+        }
+        case "LOAD_EVENTS": {
+            const ids = new Set(action.payload.events.map(event => event.id))
+            if (!ids.has(state)) {
+                return null
+            } else {
+                return state
+            }
+        }
         default: return state
     }
 }
